f37119c301fb9b47bd930e7223c9e00a8b4e3c0e
fluffy <fluffy@beesbuzz.biz>
2018-03-31 23:05:19 -0700

    Go back to using EmailMessage, because it does more stuff better

---
+++ example_site/content/foo/hidden.md
@@ -0,0 +1,6 @@
+Title: This entry is hidden
+Status: draft
+Date: 2018-03-31 22:00:56-07:00
+ID: 0
+
+This entry should not be visible.
--- publ/entry.py
+++ publ/entry.py
@@ -6,88 +6,23 @@ import os
 import re
 import arrow
 from enum import Enum
+import email
 
 import config
 
 from . import model
 from . import path_alias
 
-class ParseState(Enum):
-    HEADERS = 0
-    WHITESPACE = 1
-    ATF = 2
-    BTF = 3
+def Entry(fullpath):
+    with open(fullpath, 'r') as file:
+        entry = email.message_from_file(file)
 
-class Entry:
-    def __init__(self, fullpath):
-        # TODO this feels hacky and inelegant and there's probably a cleaner approach
-        self.headers = []
-        self.body = ''
-        self.more = ''
+    entry.body, _, entry.more = entry.get_payload().partition('~~~~~')
 
-        _,ext = os.path.splitext(fullpath)
-        self.markdown = (ext == '.md')
+    _,ext = os.path.splitext(fullpath)
+    self.is_markdown = (ext == '.md')
 
-        state = ParseState.HEADERS
-
-        # TODO handle array-type headers (references, tags, etc.)
-        # also this code feels really messy
-        with open(fullpath, 'r') as file:
-            for line in file:
-                if state == ParseState.HEADERS:
-                    m = re.match(r'([a-zA-Z0-9\-]+):\s+(.*)$', line)
-                    if m:
-                        k,v = m.group(1,2)
-                        self.headers.append((k,v))
-                    else:
-                        # We found post-header whitespace to consume
-                        state = ParseState.WHITESPACE
-
-                if state == ParseState.WHITESPACE:
-                    if line.strip():
-                        state = ParseState.ATF
-
-                # ATF processing the BTF marker doesn't fallthrough to BTF parsing
-                if state == ParseState.ATF:
-                    if line.strip() == '~~~~~':
-                        state = ParseState.BTF
-                    else:
-                        self.body += line
-                elif state == ParseState.BTF:
-                    self.more += line
-
-    ''' Get the first header matching the given key, case-insensitive '''
-    def get(self, key, default=None):
-        for k,v in self.headers:
-            if k.lower() == key.lower():
-                return v
-        return default
-
-    ''' Get a list of all headers matching the given key '''
-    def all(self, key):
-        return [v for (k,v) in self.headers if k.lower() == key.lower()]
-
-    def __getitem__(self, key):
-        return self.get(key)
-
-    ''' Replace the value of the first matching header, or add it anew '''
-    def set(self, key, val):
-        for idx,(k,v) in enumerate(self.headers):
-            if k.lower() == key.lower():
-                self.headers[idx] = (key,val)
-                return
-        self.headers.append((k,v))
-
-    def write_file(self, fullpath):
-        with open(fullpath, 'w') as file:
-            for k,v in self.headers.items():
-                # TODO camelcase the header name
-                print("{}: {}".format(k, v), file=file)
-            print('', file=file)
-            file.write(self.body)
-            if self.more:
-                print('~~~~~', file=file)
-                file.write(self.more)
+    return entry
 
 ''' convert a title into a URL-friendly slug '''
 def make_slug(title):
@@ -98,46 +33,48 @@ def scan_file(fullpath, relpath, assign_id):
     ''' scan a file and put it into the index '''
     entry = Entry(fullpath)
 
-    entry_id = int(entry['id'] or 0)
-    if not entry_id and not assign_id:
-        # We can't operate on this yet
+    entry_id = entry['ID']
+    if entry_id == None and not assign_id:
         return False
 
-    fixup_needed = not entry_id or not entry['date']
+    fixup_needed = entry_id == None or not 'Date' in entry
 
     values = {
         'file_path': fullpath,
-        'category': os.path.dirname(relpath),
-        'status': model.PublishStatus[entry['Status', 'PUBLISHED'].upper()],
-        'entry_type': model.EntryType[entry['Type', 'ENTRY'].upper()],
-        'slug_text': entry['Slug-Text'] or make_slug(entry['Title'] or os.path.basename(relpath)),
+        'category': entry.get('Category', os.path.dirname(relpath)),
+        'status': model.PublishStatus[entry.get('Status', 'PUBLISHED').upper()],
+        'entry_type': model.EntryType[entry.get('Type', 'ENTRY').upper()],
+        'slug_text': entry['Slug-Text'] or make_slug(entry.get('Title', os.path.basename(relpath))),
         'redirect_url': entry['Redirect-To'],
     }
 
-    header_date = entry['Date']
-    if header_date:
-        entry_date = arrow.get(header_date, tzinfo=config.timezone)
+    if 'Date' in entry:
+        entry_date = arrow.get(entry['Date'])
     else:
         entry_date = arrow.get(os.stat(fullpath).st_ctime).to(config.timezone)
-    entry.set_header('Date', entry_date.format())
+        entry['Date'] = entry_date.format()
     values['entry_date'] = entry_date.datetime
 
-    try:
-        # If we have entry_id, use that as the query; otherwise use fullpath
-        record = model.Entry.get(
-            entry_id and (model.Entry.id == entry_id) or
-            (model.Entry.file_path == fullpath))
+    if entry_id != None:
+        record, created = model.Entry.get_or_create(id=entry_id, defaults=values)
+    else:
+        record, created = model.Entry.get_or_create(file_path=relpath, defaults=values)
+
+    if not created:
         record.update(**values).where(model.Entry.id == record.id).execute()
-    except model.Entry.DoesNotExist:
-        record = model.Entry.create(id=entry_id, **values)
 
-    entry.set('ID', record.id)
+    # Update the entry ID
+    del entry['ID']
+    entry['ID'] = record.id
 
-    if fixup_needed:
-        entry.write_file(fullpath)
-
-    for alias in entry.all("Path-Alias"):
+    # add other relationships to the index
+    for alias in entry.get_all('Path-Alias', []):
         path_alias.set_alias(alias, entry=record)
 
-    return record
+    if fixup_needed:
+        tmpfile = fullpath + '.tmp'
+        with open(tmpfile, 'w') as file:
+            file.write(entry)
+        os.replace(tmpfile, fullpath)
 
+    return record
