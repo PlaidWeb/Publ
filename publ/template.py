0a1b77d8fbcf307490170a9efe524897219fdfd9
fluffy <fluffy@beesbuzz.biz>
2018-04-16 22:36:55 -0700

    CLean up model

--- publ/template.py
+++ publ/template.py
@@ -1,11 +1,21 @@
 # template.py
-# Wrapper for template information
+""" Wrapper for template information """
 
 import os
 import arrow
 
+
 class Template:
+    """ Template information wrapper """
+    # pylint: disable=too-few-public-methods
+
     def __init__(self, name, filename, file_path):
+        """ Useful information for the template object:
+
+        name -- The name of the template
+        filename -- The filename of the template
+        file_path -- The full path to the template
+        """
         self.name = name
         self.filename = filename
         self.last_modified = arrow.get(os.stat(file_path).st_mtime)
