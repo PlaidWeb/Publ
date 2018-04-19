ad14b1f4b2408eb3e5ce16852916d5ff251a36d7
fluffy <fluffy@beesbuzz.biz>
2018-04-19 01:02:26 -0700

    Bump to v0.1.1 to correct missing config.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='Publ',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='A content-management system for flexible web-based publishing',
 
