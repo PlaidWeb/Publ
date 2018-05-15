43c81d57d5b60e8276e7be3ecdb73c240975f716
fluffy <fluffy@beesbuzz.biz>
2018-05-15 14:16:17 -0700

    Refactor async stuff into its own module

--- publ/rendering.py
+++ publ/rendering.py
@@ -289,26 +289,3 @@ def render_entry(entry_id, slug_text='', category=''):
         tmpl,
         entry=entry_obj,
         category=Category(category)), {'Content-Type': mime_type(tmpl)}
-
-
-def async_image(filename):
-    """ Asynchronously fetch an image """
-
-    if not os.path.isfile(os.path.join(config.static_folder, filename)):
-        retry_count = int(request.args.get('retry_count', 0))
-        if retry_count < 3:
-            time.sleep(0.25)  # ghastly hack
-            return redirect(url_for('async', filename=filename, retry_count=retry_count + 1))
-
-        # the image isn't available yet; generate a placeholder and let the
-        # client attempt to re-fetch periodically, maybe
-        vals = [int(b) for b in hashlib.md5(
-            filename.encode('utf-8')).digest()[0:12]]
-        placeholder = PIL.Image.new('RGB', (2, 2))
-        placeholder.putdata(list(zip(vals[0::3], vals[1::3], vals[2::3])))
-        outbytes = io.BytesIO()
-        placeholder.save(outbytes, "PNG")
-        outbytes.seek(0)
-        return flask.send_file(outbytes, mimetype='image/png')
-
-    return redirect(url_for('static', filename=filename))
