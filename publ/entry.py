680065702866c08762b95aed260c8f0a73c5bcd9
fluffy <fluffy@beesbuzz.biz>
2018-05-25 16:24:26 -0700

    Delinting

--- publ/entry.py
+++ publ/entry.py
@@ -139,7 +139,7 @@ class Entry:
         elif paging == 'offset' or not paging:
             args['id'] = self._record.id
         else:
-            raise ValueError("Unknown paging type '%s'", paging)
+            raise ValueError("Unknown paging type '{}'".format(paging))
 
         return flask.url_for('category', **args, _external=absolute)
 
