9a5572d116a4a3890a038e6e1298b8920b5d91d1
fluffy <fluffy@beesbuzz.biz>
2018-05-30 23:05:43 -0700

    Version 0.1.21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='Publ',
 
-    version='0.1.20',
+    version='0.1.21',
 
     description='A content-management system for flexible web-based publishing',
 
