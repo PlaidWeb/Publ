7f4b28c7d8a3089bb07297c2e7ed8a97c5326cfe
fluffy <fluffy@beesbuzz.biz>
2018-07-01 21:47:33 -0700

    Don't override the static route's default cache control

--- publ/__init__.py
+++ publ/__init__.py
@@ -141,6 +141,6 @@ def startup():
 
 def set_cache_expiry(response):
     """ Set the cache control headers """
-    if 'CACHE_DEFAULT_TIMEOUT' in config.cache:
+    if response.cache_control.max_age is None and 'CACHE_DEFAULT_TIMEOUT' in config.cache:
         response.cache_control.max_age = config.cache['CACHE_DEFAULT_TIMEOUT']
     return response
