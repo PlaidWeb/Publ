fd3cff937f80c8351f023a2632d45383b98cf9c9
fluffy <fluffy@beesbuzz.biz>
2018-09-11 11:12:34 -0700

    bump to 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='Publ',
 
-    version='0.2.1',
+    version='0.2.2',
 
     description='A content-management system for flexible web-based publishing',
 
