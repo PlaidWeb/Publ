3a29df4c84918d8f0cdd0fbea6dbcd81f32d2409
fluffy <fluffy@beesbuzz.biz>
2018-09-11 11:22:00 -0700

    remove the 'quote' extension which doesn't behave as specified

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='Publ',
 
-    version='0.2.2',
+    version='0.2.2.1',
 
     description='A content-management system for flexible web-based publishing',
 
