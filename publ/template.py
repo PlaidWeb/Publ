f89d1927303af235bcef91074d579b8a88fe318f
fluffy <fluffy@beesbuzz.biz>
2018-12-16 21:16:03 -0800

    Better filename normalization for template files

--- publ/template.py
+++ publ/template.py
@@ -19,7 +19,13 @@ class Template:
         file_path -- The full path to the template
         """
         self.name = name
-        self.filename = filename
+
+        # on Windows, Flask expects template filenames to be /-separated
+        if os.sep != '/':
+            self.filename = '/'.join(os.path.normpath(filename).split(os.sep))
+        else:
+            self.filename = filename
+
         self.file_path = file_path
         self.mtime = os.stat(file_path).st_mtime
         self.last_modified = arrow.get(self.mtime)
