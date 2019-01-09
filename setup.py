3fc93f0c115bd0b3b125fff9772d1540af04c6a1
fluffy <fluffy@beesbuzz.biz>
2019-01-08 23:47:22 -0800

    Release v0.3.14

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='Publ',
 
-    version='0.3.13',
+    version='0.3.14',
 
     description='A content-management system for flexible web-based publishing',
 
