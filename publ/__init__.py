f8dee880d8c37499e0ae09393cc5da2929813d83
fluffy <fluffy@beesbuzz.biz>
2019-01-15 23:32:33 -0800

    Move version number to publ module

--- publ/__init__.py
+++ publ/__init__.py
@@ -12,6 +12,8 @@ from pony.orm import db_session
 from . import config, rendering, model, index, caching, view, utils
 from . import maintenance, image
 
+__version__ = '0.3.14'
+
 
 class _PublApp(flask.Flask):
     """ A Publ app; extends Flask so that we can add our own custom decorators """
