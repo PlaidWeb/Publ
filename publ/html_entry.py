ea9984e5b2837eb2a56accd5896e0e17bd203d4e
fluffy <fluffy@beesbuzz.biz>
2019-02-13 20:02:01 -0800

    Case-insensitivize the attribute check

--- publ/html_entry.py
+++ publ/html_entry.py
@@ -86,7 +86,8 @@ class HTMLEntry(utils.HTMLTransform):
             return [('data-publ-error', 'file not found: {}'.format(error.filename))]
 
         # return the original attr list with the computed overrides in place
-        return [(key, val) for key, val in attrs if key not in img_attrs] + list(img_attrs.items())
+        return [(key, val) for key, val in attrs
+                if key.lower() not in img_attrs] + list(img_attrs.items())
 
 
 def process(text, config, search_path):
