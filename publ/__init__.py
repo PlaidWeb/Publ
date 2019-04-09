e4fcb67c942ccaf019613ed39f311883de88639f
fluffy <fluffy@beesbuzz.biz>
2019-04-08 20:43:15 -0700

    v0.3.22

--- publ/__init__.py
+++ publ/__init__.py
@@ -10,7 +10,7 @@ import werkzeug.exceptions
 from . import config, rendering, model, index, caching, view, utils
 from . import maintenance, image
 
-__version__ = '0.3.21'
+__version__ = '0.3.22'
 
 
 class _PublApp(flask.Flask):
