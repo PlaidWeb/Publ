be5d265679074a7776c5d6a3fd4aacbb98e5132f
fluffy <fluffy@beesbuzz.biz>
2020-01-03 15:09:25 -0800

    Consolidate card parsing to post-HTML processing

--- publ/cards.py
+++ publ/cards.py
@@ -2,11 +2,8 @@
 """ Rendering functions for Twitter/OpenGraph cards"""
 
 import logging
-import typing
 
-import misaka
-
-from . import config, image, utils
+from . import utils
 
 LOGGER = logging.getLogger(__name__)
 
@@ -20,74 +17,6 @@ class CardData():
         self.images = []
 
 
-class MarkdownCardParser(misaka.BaseRenderer):
-    """ Customized Markdown renderer for parsing out information for a card """
-
-    def __init__(self, out, args, image_search_path):
-        super().__init__()
-
-        self._config = {**args, 'absolute': True}
-        self._image_search_path = image_search_path
-
-        self._out = out
-
-    def paragraph(self, content):
-        """ Turn the first paragraph of text into the summary text """
-        if not self._out.description:
-            self._out.description = content
-        return ' '
-
-    def image(self, raw_url, title='', alt=''):
-        ''' extract the images '''
-        max_images = self._config.get('count')
-        if max_images is not None and len(self._out.images) >= max_images:
-            # We already have enough images, so bail out
-            return ' '
-
-        image_specs = raw_url
-        if title:
-            image_specs += ' "{}"'.format(title)
-
-        alt, container_args = image.parse_alt_text(alt)
-
-        spec_list = image.get_spec_list(image_specs, container_args)
-
-        for (spec, show) in spec_list:
-            if not spec or not show:
-                continue
-
-            self._out.images.append(self._render_image(spec, alt))
-            if max_images is not None and len(self._out.images) >= max_images:
-                break
-
-        return ' '
-
-    @staticmethod
-    def link(content, link, title=''):
-        """ extract the text content out of a link """
-        # pylint: disable=unused-argument
-
-        return content
-
-    def _render_image(self, spec, alt=''):
-        """ Given an image spec, try to turn it into a card image per the configuration """
-        # pylint: disable=unused-argument
-
-        try:
-            path, image_args, _ = image.parse_image_spec(spec)
-        except Exception as err:  # pylint: disable=broad-except
-            # we tried™
-            LOGGER.exception("Got error on spec %s: %s", spec, err)
-            return None
-
-        img = image.get_image(path, self._image_search_path)
-        if img:
-            image_config = {**image_args, **self._config, 'absolute': True}
-            return img.get_rendition(1, **image_config)[0]
-
-        return None
-
-
 class HtmlCardParser(utils.HTMLTransform):
     """ Parse the first paragraph out of an HTML document """
 
@@ -96,12 +25,15 @@ class HtmlCardParser(utils.HTMLTransform):
 
         self._consume = True
         self._card = card
+        self._images = []
 
     def handle_starttag(self, tag, attrs):
         # pylint:disable=unused-argument
         if tag == 'p' and self._card.description:
             # We already got some data, so this is a malformed/old-style document
             self._consume = False
+        if tag == 'img':
+            self._card.images += [val for attr, val in attrs if attr == 'src']
 
     def handle_endtag(self, tag):
         if tag == 'p':
@@ -114,18 +46,9 @@ class HtmlCardParser(utils.HTMLTransform):
             self._card.description += data
 
 
-def extract_card(text: str,
-                 is_markdown: bool,
-                 args: typing.Dict[str, typing.Any],
-                 image_search_path: typing.Tuple[str]) -> CardData:
-    """ Extract card data based on the provided texts. """
+def extract_card(html_text: str) -> CardData:
+    """ Extract card data based on the provided HTML. """
     card = CardData()
-    if is_markdown:
-        misaka.Markdown(MarkdownCardParser(card, args, image_search_path),
-                        extensions=args.get('markdown_extensions')
-                        or config.markdown_extensions
-                        )(text)
-    else:
-        HtmlCardParser(card).feed(text)
+    HtmlCardParser(card).feed(html_text)
 
     return card
---
+++ tests/content/markdown-titles/leading number.md
@@ -0,0 +1,6 @@
+Title: 1. The loneliest number.
+Date: 2019-12-31 00:51:51-08:00
+Entry-ID: 2374
+UUID: 8ac9b013-4dae-52c6-a378-2436e2415cc2
+
+`1. The loneliest number.`
